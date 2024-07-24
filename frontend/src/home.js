b80b1f988ee71d64011a3d2cb0b8169881f4ae53
Fancisco Javier González Rolo <57604555+atreyu1968@users.noreply.github.com>
2024-07-24 19:17:32 +0100

    Create home.js

---
+++ frontend/src/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenido a Innovación FP</h1>
+      <p>Esta es la página principal de nuestra aplicación.</p>
+    </div>
+  );
+}
+
+export default Home;
