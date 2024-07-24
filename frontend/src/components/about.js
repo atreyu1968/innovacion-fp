4b1ba34eea4033753c433c8cbe74577c9226ee16
Fancisco Javier González Rolo <57604555+atreyu1968@users.noreply.github.com>
2024-07-24 18:42:18 +0100

    Create about.js

---
+++ frontend/src/components/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>Acerca de Innovación FP</h1>
+      <p>Esta aplicación está diseñada para gestionar acciones de innovación en centros de FP.</p>
+    </div>
+  );
+}
+
+export default About;
